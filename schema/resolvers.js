c3c4993f20cc52beaf2320a7c6879d7853a55ca4
lgklsv <kolosov20011@gmail.com>
2023-04-22 22:38:06 +0300

    feat: create resolver with arguments to query user by id

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,10 +1,16 @@
 const { UserList } = require('../fakeData');
+const _ = require('lodash');
 
 const resolvers = {
   Query: {
-    users() {
+    users: () => {
       // Here should be an Api call to the data base to get the results
       return UserList;
+    },
+    user: (parent, args) => {
+      const { id } = args;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
     },
   },
 };

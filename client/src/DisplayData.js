40972d182d88ff46458177bf02d574e156bc3e74
lgklsv <kolosov20011@gmail.com>
2023-04-24 11:24:26 +0300

    feat: fetch data on input, get movie by name

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,4 +1,4 @@
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useLazyQuery } from '@apollo/client';
 import React from 'react';
 
 const QUERY_ALL_USERS = gql`
@@ -22,9 +22,21 @@ const QUERY_ALL_MOVIES = gql`
   }
 `;
 
+const GET_MOVIE_BY_NAME = gql`
+  query Movie($name: String!) {
+    movie(name: $name) {
+      name
+      year
+    }
+  }
+`;
+
 const DisplayData = () => {
   const { data, loading, error } = useQuery(QUERY_ALL_USERS);
   const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
+  const [movieSearched, setMovieSearched] = React.useState('');
+  const [fetchMovie, { data: movieSearchedData, error: movieError }] =
+    useLazyQuery(GET_MOVIE_BY_NAME);
 
   if (loading) {
     return <h1>Data is loading...</h1>;
@@ -35,6 +47,11 @@ const DisplayData = () => {
   if (error) {
     console.log(error);
   }
+
+  if (movieError) {
+    console.log(movieError);
+  }
+
   return (
     <div>
       {data &&
@@ -53,6 +70,32 @@ const DisplayData = () => {
         movieData.movies.map((movie) => {
           return <h1>{movie.name}</h1>;
         })}
+      <div>
+        <input
+          type='text'
+          placeholder='Movie title...'
+          onChange={(e) => setMovieSearched(e.target.value)}
+        />
+        <button
+          onClick={() =>
+            fetchMovie({
+              variables: {
+                name: movieSearched,
+              },
+            })
+          }>
+          Search
+        </button>
+        <div>
+          {movieSearchedData && (
+            <div>
+              <h1>Movie name: {movieSearchedData.movie.name}</h1>
+              <h1>Movie year: {movieSearchedData.movie.year}</h1>
+            </div>
+          )}
+          {movieError && <h1>There was an error fetching the data</h1>}
+        </div>
+      </div>
     </div>
   );
 };
